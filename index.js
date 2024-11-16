dd1297f30d85ee9c1482f61aa38ef8a419e8f2f9
Harsh Patel <harshpatelh26@gmail.com>
2024-11-16 19:28:03 +0530

    revert fetaure tried

--- index.js
+++ index.js
@@ -8,31 +8,3 @@ let b=7;
 
 
 function sum(a,b) => a+b;
-
-
-for(i=1, i<10, i++){
-    console.log("My nunber is "+ i);
-}
-let abc = 24;
-console.log(abc);
-
-function newFeatAddedA(){
-    console.log('This is new feat A');
-}
-
-function newFeatAddedB(){
-    console.log('This is new feat B');
-}
-
-function newFeatAddedC(){
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-    console.log('This is new feat C');
-
-}
